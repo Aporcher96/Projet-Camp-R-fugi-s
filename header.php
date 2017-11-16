595a88da4be974c8a174bc60f6e3fcb0055a9bd3
edouard haentjens <edouard.haentjens@etud.u-picardie.fr>
2017-11-16 19:27:14 +0100

    Amélioration du template et réorganisation du site à cause du template

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <header>
-	<a href ="Index.php"><h4>Accueil</h4></a>
+	<a href ="index.php"><h4>Accueil</h4></a>
 	<?php if(isset($_SESSION['pseudo'])) { ?>
-	<a href ="Index.php"><h4>déconnexion</h4></a>
+	<a href ="index.php"><h4>déconnexion</h4></a>
 	<?php } ?>
 
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+ 
 
 </header>
